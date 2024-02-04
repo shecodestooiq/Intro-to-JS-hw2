a5ef3c29a528620a8743052d671f5738bc349dc6
amna <amnamh2000@gmail.com>
2024-02-04 03:46:03 +0300

    tash2

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
  // task 1
  var a=9;
  var b=3;
+ //task 2
+ console.log(' Is a greater than b?',a>b);
+ console.log(' Is a less than b?',a<b);
+ console.log('Is a greater than or equal to b?',a>=b);
+ console.log('Is a less than or equal to b?',a<=b);
+ console.log('Is a  equal to b?',a==b);
+ console.log(' Is a not equal to b?',a!=b);
